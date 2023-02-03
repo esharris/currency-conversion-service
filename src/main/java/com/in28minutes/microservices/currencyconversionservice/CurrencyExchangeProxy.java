8b8e2ca1a06e141ab0dbe12e457241c1bace10e1
Earl Harris <esharris@mac.com>
2023-02-03 15:47:03 -0500

    Lesson 153

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -12,9 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
  *
  * @author earlharris
  */
-@FeignClient(name="currency-exchange", url="localhost:8000")
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
-    
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
- }
+}
